b26fe6eeff3e53a228c12a27c2d5ac62aba87294
johnwesley-jb <johnwesley.jnbaptiste@gmail.com>
2023-07-12 23:38:25 -0500

    initial commit

---
+++ src/main/java/com/john/webshop/WebshopApplication.java
@@ -0,0 +1,13 @@
+package com.john.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopApplication.class, args);
+    }
+
+}
